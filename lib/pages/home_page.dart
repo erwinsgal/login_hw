2c4976343d65a3e798aa341e5d42c46d029d82ba
erwinsgal <erwinsgal@mail.ru>
2022-10-20 16:10:53 +0600

    locale pub

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,12 +15,99 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.jpg'),
+                  fit: BoxFit.cover,
+                )),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(7),
+                  child: Text(
+                    'WELCOME!',
+                    style: TextStyle(fontSize: 35, color: Colors.purple, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                LoginBox()
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+class LoginBox extends StatelessWidget {
+  const LoginBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/background.jpg'),
-          fit: BoxFit.cover,
-        )
+      width: 300,
+      height: 300,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 50),
+          const Padding(
+            padding: EdgeInsets.only(left: 25, top: 0, right: 25, bottom: 8),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.mail),
+                border: OutlineInputBorder(),
+                hintText: 'Username / Email',
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 25, top: 0, right: 25, bottom: 8),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.key),
+                border: OutlineInputBorder(),
+                hintText: 'Password',
+              ),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.purple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+
+              onPressed: () {},
+              child: const Text('Sign In'),
+            ),
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Don't have an Account?"),
+                TextButton(
+                    style: TextButton.styleFrom(
+                        primary: Colors.purple
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/registerPage');
+                    },
+                    child: const Text('Sign Up'))
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
